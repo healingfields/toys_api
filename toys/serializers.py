dedface77ef1e535c8b496472164a3017445983d
healingfields <dihratene@gmail.com>
2022-06-16 16:11:45 +0100

    updated toy serializer

--- toys/serializers.py
+++ toys/serializers.py
@@ -2,11 +2,12 @@ from rest_framework import serializers
 from .models import Toy
 
 class ToySerializer(serializers.Serializer):
+
   pk = serializers.IntegerField(read_only=True)
   name = serializers.CharField(max_length=100)
   description = serializers.CharField(max_length=250)
-  release_date = serializers.DateTimeField()
-  toy_category = serializers.CharField()
+  release_date = serializers.DateTimeField(format=None)
+  toy_category = serializers.CharField(max_length=200)
   was_home = serializers.BooleanField(required=False)
 
   def create(self, validated_data):
@@ -15,8 +16,8 @@ class ToySerializer(serializers.Serializer):
   def update(self, instance, validated_data):
     instance.name = validated_data.get('name', instance.name)
     instance.description = validated_data.get('description', instance.description)
-    instance.release_date = validated_data.get('release_date', instance.release_date),
-    instance.toy_categroy = validated_data.get('toy_category', instance.toy_category)
+    instance.release_date = validated_data.get('release_date', instance.release_date)
+    instance.toy_category = validated_data.get('toy_category', instance.toy_category)
     instance.was_home = validated_data.get('was_home', instance.was_home)
     instance.save()
     return instance
